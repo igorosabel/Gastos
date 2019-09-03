e0339c57b70b367073c3b5663da3dfc94a553a0a
Iñigo Gorosabel Fernandez <inigo.gorosabel@gmail.com>
2019-09-03 20:12:38 +0200

    Estructura y dependencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule }           from '@angular/platform-browser';
 import { NgModule }                from '@angular/core';
 import { ServiceWorkerModule }     from '@angular/service-worker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule }        from '@angular/common/http';
 import { FormsModule }             from '@angular/forms';
 import { AppRoutingModule }        from './app-routing.module';
 import { AppComponent }            from './app.component';
 import { environment }             from '../environments/environment';
+import { TokenInterceptor }        from './interceptors/token.interceptor';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MatFormFieldDefaultOptions, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 import { PAGES, COMPONENTS, PIPES, SERVICES, MATERIAL } from './app.common';
 
@@ -31,17 +33,21 @@ const appearance: MatFormFieldDefaultOptions = {
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule,
     BrowserAnimationsModule,
     ...MATERIAL
   ],
   entryComponents: [ConfirmDialogComponent, AlertDialogComponent, FormDialogComponent],
   providers: [
-	{
+    {
       provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
       useValue: appearance
     },
-    ...SERVICES
+    ...SERVICES,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,20 +3,9 @@ import { Injectable }              from '@angular/core';
 import { Observable }              from 'rxjs';
 import { environment }             from '../../environments/environment';
 
-import {
-  StartDataResult,
-  AppData,
-  StatusResult,
-  Marca,
-  MarcasResult,
-  Proveedor,
-  ProveedoresResult,
-  IdSaveResult,
-  CategoriasResult,
-  Articulo,
-  ArticuloSaveResult,
-  ArticuloResult
-} from '../interfaces/interfaces';
+/*import {
+  StatusResult
+} from '../interfaces/interfaces';*/
 
 @Injectable({
   providedIn: 'root'
@@ -26,47 +15,7 @@ export class ApiService {
 
   constructor(private http : HttpClient){}
 
-  checkStart(date: string): Observable<StartDataResult> {
+  /*checkStart(date: string): Observable<StartDataResult> {
     return this.http.post<StartDataResult>(this.apiUrl + 'checkStart', {date});
-  }
-  
-  saveInstallation(data: AppData): Observable<StatusResult> {
-    return this.http.post<StatusResult>(this.apiUrl + 'saveInstallation', data);
-  }
-  
-  openBox(): Observable<StatusResult> {
-    return this.http.post<StatusResult>(this.apiUrl + 'openBox', {});
-  }
-  
-  getMarcas(): Observable<MarcasResult> {
-    return this.http.post<MarcasResult>(this.apiUrl + 'getMarcas', {});
-  }
-  
-  getProveedores(): Observable<ProveedoresResult> {
-    return this.http.post<ProveedoresResult>(this.apiUrl + 'getProveedores', {});
-  }
-  
-  getCategorias(): Observable<CategoriasResult> {
-    return this.http.post<CategoriasResult>(this.apiUrl + 'getCategorias', {});
-  }
-  
-  disableProduct(id: number): Observable<StatusResult> {
-    return this.http.post<StatusResult>(this.apiUrl + 'disableProduct', {id});
-  }
-  
-  saveMarca(marca: Marca): Observable<IdSaveResult> {
-    return this.http.post<IdSaveResult>(this.apiUrl + 'saveMarca', marca);
-  }
-  
-  saveProveedor(proveedor: Proveedor): Observable<IdSaveResult> {
-    return this.http.post<IdSaveResult>(this.apiUrl + 'saveProveedor', proveedor);
-  }
-  
-  saveArticulo(articulo: Articulo): Observable<ArticuloSaveResult> {
-    return this.http.post<ArticuloSaveResult>(this.apiUrl + 'saveArticulo', articulo);
-  }
-  
-  loadArticulo(localizador: number): Observable<ArticuloResult> {
-    return this.http.post<ArticuloResult>(this.apiUrl + 'loadArticulo', {localizador});
-  }
+  }*/
 }
