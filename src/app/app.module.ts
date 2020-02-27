246905c174aca61b015e15cdb61f4e2d3e5d4248
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2020-02-27 18:48:10 +0100

    Actualizo Angular Material (9.0.1 -> 9.1.0), Angular CDK (9.0.1 -> 9.1.0) y quito código no necesario con Angular 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,10 +12,6 @@ import { MatFormFieldDefaultOptions, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@ang
 
 import { PAGES, COMPONENTS, PIPES, SERVICES, MATERIAL } from './app.common';
 
-import { ConfirmDialogComponent } from './components/dialogs/confirm-dialog/confirm-dialog.component';
-import { AlertDialogComponent }   from './components/dialogs/alert-dialog/alert-dialog.component';
-import { FormDialogComponent }    from './components/dialogs/form-dialog/form-dialog.component';
-
 const appearance: MatFormFieldDefaultOptions = {
   appearance: 'outline'
 };
@@ -36,7 +32,6 @@ const appearance: MatFormFieldDefaultOptions = {
     BrowserAnimationsModule,
     ...MATERIAL
   ],
-  entryComponents: [ConfirmDialogComponent, AlertDialogComponent, FormDialogComponent],
   providers: [
     {
       provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
