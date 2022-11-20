ee92f415809ee77a3ab8988bf4b5e00f43d425ba
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2022-11-20 15:41:17 +0100

    Actualizo Angular CLI  (14.2.9 -> 15.0.0), Angular (14.2.10 -> 15.0.0), Angular Material (14.2.7 -> 15.0.0) y  Angular CDK (14.2.7 -> 15.0.0). Correcciones por cambios de diseño.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
-import { NgModule }             from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent }       from './pages/login/login.component';
-import { RegisterComponent }    from './pages/register/register.component';
-import { MainComponent }        from './pages/main/main.component';
-import { AuthGuard }            from './guard/auth.guard';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./pages/login/login.component";
+import { RegisterComponent } from "./pages/register/register.component";
+import { MainComponent } from "./pages/main/main.component";
+import { AuthGuard } from "./guard/auth.guard";
 
 const routes: Routes = [
-  { path: '',         component: LoginComponent },
-	{ path: 'register', component: RegisterComponent },
-	{ path: 'main',     component: MainComponent,    canActivate: [AuthGuard] },
-	{ path: '**', redirectTo: '/', pathMatch: 'full' }
+  { path: "", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "main", component: MainComponent, canActivate: [AuthGuard] },
+  { path: "**", redirectTo: "/", pathMatch: "full" },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
